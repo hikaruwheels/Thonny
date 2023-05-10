07333cdb78b7350bf85741a62f5e5c297c5d1567
Darwin <114919297+hikaruwheels@users.noreply.github.com>
2023-05-10 13:19:24 +0200

    Update comparacion_palabras.py

--- comparacion_palabras.py
+++ comparacion_palabras.py
@@ -3,3 +3,7 @@ def comparacion(palabra1, palabra2):
         return "Son palabras iguales"
     else:
         return "No son iguales"
+palabra1 = str(input("Dime una palabra "))
+palabra2 = str(input("Dime otra palabra "))
+
+print (comparacion(palabra1, palabra2))
